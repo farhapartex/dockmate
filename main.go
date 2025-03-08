e669cf842d411876bb53865dd3a73f1df1c4070b
Md Nazmul Hasan <farhapartex@gmail.com>
2025-03-09 00:02:58 +0600

    (feat) implemented function to create django project

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -18,6 +19,8 @@ func main() {
 	redis := flag.String("redis", "", "Specifiy Redis: redis")
 	celery := flag.String("celery", "", "Specifiy Celery: celery")
 	port := flag.Int("port", 8000, "Server exposed port")
+	venv := flag.String("venv", "venv", "Virtual environment name")
+	pyve := flag.String("pyve", "python3.10", "Python version")
 
 	flag.Parse()
 
@@ -26,13 +29,13 @@ func main() {
 	}
 
 	framework = toLower(framework)
-	generateProject(framework, db, redis, celery, port)
+	generateProject(framework, db, redis, celery, port, venv, pyve)
 }
 
-func generateProject(framework *string, db *string, redis *string, celery *string, port *int) {
+func generateProject(framework *string, db *string, redis *string, celery *string, port *int, venv *string, pyve *string) {
 	switch *framework {
 	case "django":
-		generateDjangoProject()
+		generateDjangoProject(pyve, venv)
 	case "fastapi":
 		generateFastAPIProject()
 	case "flask":
@@ -42,8 +45,58 @@ func generateProject(framework *string, db *string, redis *string, celery *strin
 	createDockerfile(*port)
 }
 
-func generateDjangoProject() {
-	fmt.Println("Django")
+func deleteFolderIfExists(dirName *string) {
+	if _, err := os.Stat(*dirName); err == nil {
+		fmt.Println("Project directory already exists. Removing...")
+		if err := os.RemoveAll(*dirName); err != nil {
+			fmt.Println("Error removing existing project directory:", err)
+			return
+		}
+	}
+
+}
+
+func generateDjangoProject(pyve *string, venv *string) {
+	projectDir := "dockmake_django"
+	deleteFolderIfExists(&projectDir)
+
+	err := os.Mkdir(projectDir, 0755)
+	if err != nil {
+		fmt.Println("Error to create django project root dir: ", err)
+		return
+	}
+
+	cmd := exec.Command(*pyve, "-m", "venv", *venv)
+	cmd.Dir = projectDir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("Error creating virtual environment:", err)
+		return
+	}
+
+	installCmd := exec.Command("bash", "-c", "source venv/bin/activate && pip install django djangorestframework")
+	installCmd.Dir = projectDir
+	installCmd.Stdout = os.Stdout
+	installCmd.Stderr = os.Stderr
+	if err := installCmd.Run(); err != nil {
+		fmt.Println("Error installing Django and DRF:", err)
+		return
+	}
+	fmt.Println("Django and DRF installed successfully!")
+
+	djangoCmd := exec.Command("bash", "-c", "source venv/bin/activate && django-admin startproject core .")
+	djangoCmd.Dir = projectDir
+	djangoCmd.Stdout = os.Stdout
+	djangoCmd.Stderr = os.Stderr
+	if err := djangoCmd.Run(); err != nil {
+		fmt.Println("Error creating Django project:", err)
+		return
+	}
+	fmt.Println("Django core project created successfully!")
+
 }
 
 func generateFastAPIProject() {
